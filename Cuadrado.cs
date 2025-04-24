68eb286373e2f9f4e37321926de36f9686b9d0e0
Arceusblocks <cristianperezpuelles@gmail.com>
2025-04-24 16:24:10 -0500

    Se agregaron nuevas formas geometricas

---
+++ Cuadrado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase
+{
+    internal class Cuadrado
+    {
+        private float b;
+        private float h;
+        public Cuadrado(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public float CalcularArea()
+        {
+            return b * h;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -105,6 +105,8 @@ namespace Clase
                     }
                 }
             }
+
+            
         }
     }
 }
